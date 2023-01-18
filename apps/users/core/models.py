7265819c845b9defe0c75b79dc3b56a6a87b1bab
philipkogel <philipkogel@yahoo.pl>
2023-01-18 21:18:29 +0100

    feat(UsersMicroservice): users project added, User model implemented and docker-compose configured

---
+++ apps/users/core/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+class UserManager(BaseUserManager):
+  def create_user(self, email: str, password: str = None, **extra_fields):
+    """Create save and return a new user."""
+    if not email:
+      raise ValueError("User must have an email!")
+    if not password:
+      raise ValueError("User must have an password!")
+
+    user = self.model(
+      email=self.normalize_email(email),
+      **extra_fields
+    )
+    user.set_password(password)
+    user.is_admin=False
+    user.is_staff=False
+    user.is_ambassador=False
+    user.save(using=self.db)
+
+    return user
+
+class User(AbstractUser):
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  email = models.EmailField(max_length=255, unique=True)
+  password = models.CharField(max_length=255)
+  is_ambassador = models.BooleanField(default=True)
+  username = None
+
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = []
+
+  objects = UserManager()
+
+  @property
+  def name(self):
+    """Return users full name"""
+    return f"{self.first_name} {self.last_name}"
+
+  # @property
+  # def revenue(self):
+  #   """Return users revenue"""
+  #   orders =
